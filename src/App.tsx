396052c88a19f29e89333853e41f9e679be569cd
Johannes Goslar <johannes@sphere.me>
2020-06-26 23:23:56 +0200

    shaker

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
-import React from 'react'
+import React, { useCallback } from 'react'
 import './App.css'
 
 import { Client as Styletron } from 'styletron-engine-atomic'
 import { Provider as StyletronProvider } from 'styletron-react'
 import { LightTheme, BaseProvider, styled } from 'baseui'
 import { StatefulInput } from 'baseui/input'
+import useShaked from './useShaked'
+
 const engine = new Styletron()
 
 const Centered = styled('div', {
@@ -16,13 +18,19 @@ const Centered = styled('div', {
 })
 
 export function App() {
+  const shakeHandler = useCallback(() => {
+    console.log('shaked')
+  }, [])
+
+  const [wasShaked] = useShaked(shakeHandler)
+
   return (
     <StyletronProvider value={engine}>
       <BaseProvider theme={LightTheme}>
         <Centered>
           <h1>Shange the World!</h1>
           <StatefulInput />
-          <h2>yeah!</h2>
+          {wasShaked && <h1>yeah baby!</h1>}
         </Centered>
       </BaseProvider>
     </StyletronProvider>
