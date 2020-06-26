5bc5b3a74ea11361d938fbc9350d3a7c9ba632d8
ksjogo <ksjogo@users.noreply.github.com>
2020-06-26 20:37:59 +0000

    deploy: 6724148af1d1851a88cb138831c9051338febf31

---
+++ precache-manifest.d78c9371b762db7b449ef0680d0cd092.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0fe5fad2d5715534608e6f5931e7bc81",
+    "url": "/2020climatehack/index.html"
+  },
+  {
+    "revision": "ea20c2ea15201bb0d47e",
+    "url": "/2020climatehack/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "05fd0978bf9f1bd181cd",
+    "url": "/2020climatehack/static/js/2.08a0e9f6.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/2020climatehack/static/js/2.08a0e9f6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ea20c2ea15201bb0d47e",
+    "url": "/2020climatehack/static/js/main.e5ddee60.chunk.js"
+  },
+  {
+    "revision": "ae075959b66539478983",
+    "url": "/2020climatehack/static/js/runtime-main.f314b74c.js"
+  }
+]);
