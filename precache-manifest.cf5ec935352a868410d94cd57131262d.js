b29848ce00e94be840212df27d45b51d8ad877c9
ksjogo <ksjogo@users.noreply.github.com>
2020-06-28 01:36:20 +0000

    deploy: 2950f581a9d192fc2b93e1d6d2eca679039ea94b

---
+++ precache-manifest.cf5ec935352a868410d94cd57131262d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6eb16e0127f54f71fdcbbe33fe8e2015",
+    "url": "/2020climatehack/index.html"
+  },
+  {
+    "revision": "d04e51f325a16f8a21fb",
+    "url": "/2020climatehack/static/css/main.e155234e.chunk.css"
+  },
+  {
+    "revision": "3ec040f79a0258c20147",
+    "url": "/2020climatehack/static/js/2.05032df8.chunk.js"
+  },
+  {
+    "revision": "731747a2d326a62dc9178f3b3c62c727",
+    "url": "/2020climatehack/static/js/2.05032df8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d04e51f325a16f8a21fb",
+    "url": "/2020climatehack/static/js/main.78acae6f.chunk.js"
+  },
+  {
+    "revision": "ae075959b66539478983",
+    "url": "/2020climatehack/static/js/runtime-main.f314b74c.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/2020climatehack/precache-manifest.b4de2a310214bebd532178d95d39fb85.js"
+  "/2020climatehack/precache-manifest.cf5ec935352a868410d94cd57131262d.js"
 );
 
 self.addEventListener('message', (event) => {
